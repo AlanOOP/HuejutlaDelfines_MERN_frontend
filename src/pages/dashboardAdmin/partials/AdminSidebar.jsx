f2eabfc56c0674e8c221f999829c951dfa2195ed
AlanAlexis <delealli7js@gmail.com>
2023-10-25 22:41:29 -0600

    redirects routes

--- src/pages/dashboardAdmin/partials/AdminSidebar.jsx
+++ src/pages/dashboardAdmin/partials/AdminSidebar.jsx
@@ -58,7 +58,7 @@ const AdminSidebar = () => {
               />
             </svg>
           </span>
-          <span className="hover:text-gray-800">Categories</span>
+          <span className="hover:text-gray-800">Cursos</span>
         </div>
         <hr className="border-b border-gray-200" />
         <div
@@ -85,7 +85,34 @@ const AdminSidebar = () => {
               />
             </svg>
           </span>
-          <span className="hover:text-gray-800">Product</span>
+          <span className="hover:text-gray-800">Competencias</span>
+        </div>
+        <hr className="border-b border-gray-200" />
+        <div
+          onClick={(e) => history.push("/admin/dashboard/orders")}
+          className={`${
+            location.pathname === "/admin/dashboard/orders"
+              ? "border-r-4 border-gray-800 bg-gray-100"
+              : ""
+          } hover:bg-gray-200 cursor-pointer flex flex-col items-center justify-center py-6`}
+        >
+          <span>
+            <svg
+              className="w-8 h-8 text-gray-600 hover:text-gray-800"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+              />
+            </svg>
+          </span>
+          <span className="hover:text-gray-800">Horarios</span>
         </div>
         <hr className="border-b border-gray-200" />
         <div
@@ -115,6 +142,7 @@ const AdminSidebar = () => {
           <span className="hover:text-gray-800">Order</span>
         </div>
         <hr className="border-b border-gray-200" />
+        
       </div>
   )
 }
