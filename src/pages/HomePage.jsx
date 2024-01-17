5bc637f3a7b18b2d2986cb760908bacc2d59e401
AlanAlexis <delealli7js@gmail.com>
2024-01-16 22:34:13 -0600

    eliminacion de env

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,6 +12,16 @@ const HomePage = () => {
       />
 
       <CoursesHome />
+
+      <div className="container flex justify-center items-center m-5">
+        <iframe width="640" height="480"
+          src="https://sketchfab.com/models/de9e202843494872bfa5c0c2192efea2/embed?autostart=1&internal=1&tracking=0&ui_ar=0&ui_infos=0&ui_snapshots=1&ui_stop=0&ui_theatre=1&ui_watermark=0"
+          xr-spatial-tracking="true" exection-while-out-of-viewport="true" execution-while-not-rendered="true"
+          web-share="true" allow="autoplay; fullscreen; vr" allowfullscreen="true" mozallowfullscreen="true"
+        >
+
+        </iframe>
+      </div>
     </Layout>
   )
 }
